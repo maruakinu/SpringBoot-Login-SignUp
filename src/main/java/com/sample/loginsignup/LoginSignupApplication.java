9e1315b3310a3181d7f92d6666cced8516eb1bfd
maruakinu <marloaquino080621@gmail.com>
2023-07-21 10:53:08 +0800

    Project Initialization

---
+++ src/main/java/com/sample/loginsignup/LoginSignupApplication.java
@@ -0,0 +1,13 @@
+package com.sample.loginsignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSignupApplication.class, args);
+	}
+
+}
