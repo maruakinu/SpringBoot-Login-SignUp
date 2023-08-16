f2e2c5885c2e31c483f932ea4763795239ded6a6
maruakinu <marloaquino080621@gmail.com>
2023-08-16 19:22:57 +0800

    Adding Web Security and Password Encoder

--- src/main/java/com/sample/loginsignup/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/sample/loginsignup/domain/user/service/UserServiceImpl.java
@@ -6,9 +6,9 @@ import com.sample.loginsignup.domain.user.entity.UserEntity;
 import com.sample.loginsignup.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import com.sample.loginsignup.domain.exception.Error;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +17,8 @@ public class UserServiceImpl implements UserService{
     @Autowired
     private UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public UserDto registration(UserDto.Registration registration) {
         userRepository.findByUsername(registration.getUsername()).stream().findAny().ifPresent(entity -> {
@@ -24,7 +26,7 @@ public class UserServiceImpl implements UserService{
         });
         UserEntity userEntity = UserEntity.builder()
                 .username(registration.getUsername())
-                .password(registration.getPassword())
+                .password(passwordEncoder.encode(registration.getPassword()))
                 .build();
 
         if (registration.getUsername() == "" || registration.getPassword() == ""){
